ba7adcb6ff2078558cbd8c2fd8bf2f0de166cb7f
Mattia Cavalieri Manasse <cavalierimanasse.2010869@strudenti.uniroma1.it>
2025-02-01 23:53:28 +0100

    episodio 4 added

---
+++ src/episodio4/Applicazione.java
@@ -0,0 +1,9 @@
+package episodio4;
+
+public class Applicazione {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+
+    }
+}
---
+++ src/episodio4/Frame.java
@@ -0,0 +1,45 @@
+package episodio4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Frame extends JFrame {
+
+    private JTextArea textArea;
+    private JTextField textField;
+    private JButton button;
+
+    public Frame() {
+        super("La mia prima finestra");
+
+        setLayout(new BorderLayout());
+
+        textArea = new JTextArea();
+        textField = new JTextField();
+        button = new JButton("Cliccami");
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String testoTextField = textField.getText();
+                if (!testoTextField.equals("")) {
+                    textArea.append(testoTextField + "\n");
+                    textField.setText("");
+                }
+
+            }
+        });
+
+        add(textArea, BorderLayout.CENTER);
+        add(textField, BorderLayout.PAGE_START);
+        add(button, BorderLayout.PAGE_END);
+
+        setSize(1000, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+}
